309f7823ad572aab295dfb9449be3c6292d906a4
YimingJ <jiangyimings@gmail.com>
2017-10-02 13:51:08 +1100

    change timer mode

--- Timer/app/src/main/java/com/timer/jiangyiming/timer/MainActivity.java
+++ Timer/app/src/main/java/com/timer/jiangyiming/timer/MainActivity.java
@@ -4,25 +4,42 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private Button timerOn;
     private static final String KEY = "timer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         MessageReceiver receiver = new MessageReceiver(new Message());
 
-
         Intent intent = new Intent(this, TimerService.class);
-        intent.putExtra("time", 100);
+        intent.putExtra("time", 10);
         intent.putExtra("receiver", receiver);
         startService(intent);
+        changeTimerMode(intent);
+    }
+    public void changeTimerMode(final Intent intent){
+        timerOn = (Button) findViewById(R.id.timerOn);
+        timerOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (timerOn.getText().equals("timerOn")){
+                    timerOn.setText("timerOff");
+                    stopService(intent);
+                }else{
+                    timerOn.setText("timerOn");
+                    startService(intent);
+                }
+            }
+        });
     }
 
     public class Message {
--- Timer/app/src/main/java/com/timer/jiangyiming/timer/TimerService.java
+++ Timer/app/src/main/java/com/timer/jiangyiming/timer/TimerService.java
@@ -11,6 +11,7 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.ResultReceiver;
+import android.provider.Settings;
 import android.util.Log;
 
 /**
@@ -18,7 +19,10 @@ import android.util.Log;
  */
 
 public class TimerService extends IntentService {
-    private static final int UniqueID = 1124;
+//    private static final long timeInterval = 1000 * 60 * 120;
+    private static final long timeInterval = 4000;
+    private long startTime;
+    private boolean stopFlag=false;
 
     public TimerService() {
         super("Timer Service");
@@ -27,6 +31,7 @@ public class TimerService extends IntentService {
     @Override
     public void onCreate() {
         super.onCreate();
+        startTime = System.currentTimeMillis();
         Log.d("lovelytimer", "Timer service starts");
 
     }
@@ -39,60 +44,67 @@ public class TimerService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent == null) {
-            int time = 5;
-            for (int i = 0; i < time; i++) {
-                Log.d("lovelytimer", "i (intent is null)= " + i);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
 
-            String id = "myChannel";
-            int importance = NotificationManager.IMPORTANCE_LOW;
-            NotificationChannel myChannel = null;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                myChannel = new NotificationChannel(id, "1234", importance);
-                myChannel.setLightColor(Color.GREEN);
-            }
-            NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                nm.createNotificationChannel(myChannel);
-            }
-
-            Notification notification = null;
-            Intent intent1 = new Intent(this, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                notification = new Notification.Builder(this,id)
-                        .setContentTitle("Hey~Hey~Hey")
-                        .setContentText("Time for exsercise now!!")
-                        .setSmallIcon(R.mipmap.ic_launcher)
-                        .setAutoCancel(true)
-                        .setContentIntent(pendingIntent)
-                        .setShowWhen(true)
-                        .build();
-            }
-            nm.notify(1,notification);
-
-            return;
-        }
-
-        ResultReceiver receiver = intent.getParcelableExtra("receiver");
-        int time = intent.getIntExtra("time", 0);
-        for (int i = 0; i < time; i++) {
-            Log.d("lovelytimer", "i (intent is not null) =" + i);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        while (!stopFlag) {
+            long interval = System.currentTimeMillis() - startTime;
+            if (interval > timeInterval) {
+                Log.d("lovelytimer", ""+interval);
+                sendNotification();
+                startTime = System.currentTimeMillis();
             }
         }
-        Bundle bundle = new Bundle();
-        bundle.putString("finish", "counting finished");
-        receiver.send(1234, bundle);
+
+//        ResultReceiver receiver = intent.getParcelableExtra("receiver");
+//        int time = intent.getIntExtra("time", 0);
+//        for (int i = 0; i < time; i++) {
+//            Log.d("lovelytimer", "i (intent is not null) =" + i);
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        Bundle bundle = new Bundle();
+//        bundle.putString("finish", "counting finished");
+//        receiver.send(1234, bundle);
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopFlag=true;
+        Log.d("lovelytimer","you are not lovely at all");
+
+    }
+
+    public void sendNotification() {
+        String id = "myChannel";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel myChannel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            myChannel = new NotificationChannel(id, "1234", importance);
+            myChannel.setLightColor(Color.GREEN);
+        }
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            nm.createNotificationChannel(myChannel);
+        }
+
+        Notification notification = null;
+        Intent intent1 = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notification = new Notification.Builder(this, id)
+                    .setContentTitle("Hey~Hey~Hey")
+                    .setContentText("Time for exsercise now!!")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setShowWhen(true)
+                    .build();
+        }
+        nm.notify(1, notification);
+    }
+
 }
