6609421b74154ebfd2a21ac1220181bc091a4d10
YimingJ <jiangyimings@gmail.com>
2017-10-02 14:23:57 +1100

    combine sensor and timer

--- Timer/app/src/main/java/com/timer/jiangyiming/timer/MainActivity.java
+++ Timer/app/src/main/java/com/timer/jiangyiming/timer/MainActivity.java
@@ -1,15 +1,23 @@
 package com.timer.jiangyiming.timer;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    private TextView xText,yText,zText;
+    private Sensor mySensor;
+    private SensorManager sensorManager;
 
     private Button timerOn;
     private static final String KEY = "timer";
@@ -19,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MessageReceiver receiver = new MessageReceiver(new Message());
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        //accelerometer
+        mySensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //sensor listener
+        sensorManager.registerListener(this,mySensor,SensorManager.SENSOR_DELAY_NORMAL);
+
+        //assign textviews
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+
 
         Intent intent = new Intent(this, TimerService.class);
         intent.putExtra("time", 10);
@@ -40,6 +60,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        xText.setText("X "+ sensorEvent.values[0]);
+        yText.setText("Y "+ sensorEvent.values[1]);
+        zText.setText("Z "+ sensorEvent.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 
     public class Message {
