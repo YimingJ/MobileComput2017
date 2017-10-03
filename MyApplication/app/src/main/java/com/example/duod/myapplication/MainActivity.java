7ba3c3e3a648a39ef42f541a2e3c4fff259242ea
Ning Ma <ningm1@student.unimelb.edu.au>
2017-10-03 15:42:40 +1100

    rename

--- MyApplication/app/src/main/java/com/example/duod/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/duod/myapplication/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.duod.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,11 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=(Button)findViewById(R.id.button);
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            Intent intent=new Intent(MainActivity.this,Main2Activity.class);
+            Intent intent=new Intent(MainActivity.this,SignIn.class);
                 startActivity(intent);
             }
         });
