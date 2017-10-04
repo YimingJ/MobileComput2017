df357509142e9ec3b7a9effea564ae46807d854d
Ning Ma <ningm1@student.unimelb.edu.au>
2017-10-05 00:41:44 +1100

    exercise

---
+++ MyApplication/app/src/main/java/com/example/duod/myapplication/Exercise.java
@@ -0,0 +1,33 @@
+package com.example.duod.myapplication;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class Exercise extends AppCompatActivity {
+    VideoView videoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+        videoView=(VideoView)findViewById(R.id.videoView);
+        String videoUrl="https://youtu.be/1919eTCoESo";
+        Uri uri=Uri.parse(videoUrl);
+        videoView.setMediaController(new MediaController(this));
+        videoView.setOnCompletionListener(new PlayerOnCompletionListener());
+        videoView.setVideoURI(uri);
+        videoView.start();
+        videoView.requestFocus();
+    }
+    class PlayerOnCompletionListener implements MediaPlayer.OnCompletionListener{
+        public void onCompletion(MediaPlayer mediaPlayer){
+            Toast.makeText(Exercise.this,"plays",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- MyApplication/app/src/main/java/com/example/duod/myapplication/MainInterface.java
+++ MyApplication/app/src/main/java/com/example/duod/myapplication/MainInterface.java
@@ -1,13 +1,43 @@
 package com.example.duod.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.security.Policy;
 
 public class MainInterface extends AppCompatActivity {
-
+    Button homeButton,exceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_interface);
+        homeButton=(Button)findViewById(R.id.button7);
+        exceButton=(Button)findViewById(R.id.button8);
+        exceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainInterface.this,Exercise.class);
+                startActivity(intent);
+            }
+        });
+        //homeButton.setCompoundDrawables(null,null,findImage(MainInterface.this,R.drawable.homeButton, Parameters.IMG_SMALL),null,null);
     }
+   /* public static Drawable findImage(Context context,int id, int side){
+        Drawable drawable= ContextCompat.getDrawable(context, id);
+        int px= ViewDragHelperHelper.dip(context,side);
+        drawable.setBounds(0,0,px,px);
+        return drawable;
+
+    }
+    public static int dip(Context context,float dip){
+        final float scale=context.getResources().getDisplayMetrics().density;
+        return (int)(dip*scale+0.5f);
+    }*/
 }
