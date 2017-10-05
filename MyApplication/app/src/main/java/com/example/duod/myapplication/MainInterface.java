38e20bbffd8dd54dd91583716a775a6e7e0f234b
Ning Ma <ningm1@student.unimelb.edu.au>
2017-10-05 20:38:40 +1100

    four kinds exercises

--- MyApplication/app/src/main/java/com/example/duod/myapplication/MainInterface.java
+++ MyApplication/app/src/main/java/com/example/duod/myapplication/MainInterface.java
@@ -9,39 +9,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.security.Policy;
 
 public class MainInterface extends AppCompatActivity {
-    TextView me,exercise;
+    TextView me,exercise,loseweight;
     LinearLayout linearLayout;
+    ImageView loseWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_interface);
-       linearLayout =(LinearLayout)findViewById(R.id.linearLayout);
+        linearLayout =(LinearLayout)findViewById(R.id.linearLayout);
         me=(TextView)findViewById(R.id.textView3);
         exercise=(TextView)findViewById(R.id.textView2);
-//        welcome=(TextView)findViewById(R.id.textView4);
-//        Intent intent=new Intent();
-//        String username = getIntent().getStringExtra("time");
-//        String username=intent.getStringExtra("time");
-//        welcome.setText("Welcome Back"+username);
+        loseweight=(TextView)findViewById(R.id.textView4);
+        loseWeight=(ImageView)findViewById(R.id.imageView2);
 
-        //homeButton.setCompoundDrawables(null,null,findImage(MainInterface.this,R.drawable.homeButton, Parameters.IMG_SMALL),null,null);
-    }
-   /* public static Drawable findImage(Context context,int id, int side){
-        Drawable drawable= ContextCompat.getDrawable(context, id);
-        int px= ViewDragHelperHelper.dip(context,side);
-        drawable.setBounds(0,0,px,px);
-        return drawable;
-
-    }
-    public static int dip(Context context,float dip){
-        final float scale=context.getResources().getDisplayMetrics().density;
-        return (int)(dip*scale+0.5f);
-    }*/
-}
+        loseWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent=new Intent(MainInterface.this,LoseWeight.class);
+                    startActivity(intent);}
+        });
+        loseweight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainInterface.this,LoseWeight.class);
+                startActivity(intent);}
+        });
+}}
