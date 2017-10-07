c012f60e379ce82ae5c092b38b0a72ff6eb32260
YimingJ <jiangyimings@gmail.com>
2017-10-07 16:19:46 +1100

    add database connection

--- MoveUp/app/src/main/java/com/example/moveup/Login.java
+++ MoveUp/app/src/main/java/com/example/moveup/Login.java
@@ -6,19 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.val;
 
 public class Login extends Activity {
-    Button buttonCancel,buttonOk;
-    EditText lEmail,lPwd;
+    Button buttonCancel, buttonOk;
+    EditText lEmail, lPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        buttonCancel=(Button)findViewById(R.id.button5);
-        buttonOk=(Button)findViewById(R.id.button6);
-        lEmail=(EditText)findViewById(R.id.editText3);
-        lPwd=(EditText)findViewById(R.id.editText6);
+        buttonCancel = (Button) findViewById(R.id.button5);
+        buttonOk = (Button) findViewById(R.id.button6);
+        lEmail = (EditText) findViewById(R.id.editText3);
+        lPwd = (EditText) findViewById(R.id.editText6);
+        final String userEmail = lEmail.getText().toString().trim();
+        final String userPsw = lPwd.getText().toString().trim();
         buttonOk.setHeight(100);
         buttonOk.setWidth(350);
         buttonCancel.setHeight(100);
@@ -26,23 +34,36 @@ public class Login extends Activity {
         buttonCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,MainActivity.class);
+                Intent intent = new Intent(Login.this, MainActivity.class);
                 startActivity(intent);
             }
         });
         buttonOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(IsCorrect()){
-                Intent intent=new Intent(Login.this,MainInterface.class);
-                startActivity(intent);}
+                if (IsCorrect(userEmail, userPsw)) {
+                    Intent intent = new Intent(Login.this, MainInterface.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(Login.this,"Please type in your correct email and password",Toast.LENGTH_SHORT).show();
+                }
             }
 
         });
     }
 
-    public boolean IsCorrect(){
+    public boolean IsCorrect(String userEmail, String userPsw) {
         //// TODO: 2017/10/3 1.database
-        return true;
+        try {
+            List<User> result = MoveUpConstant.getInstance().getUserTable().where().field("email").eq(val(userEmail)).execute().get();
+            if (result.size() == 1 && result.get(0).getuPwd().equals(userPsw)) {
+                return true;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
