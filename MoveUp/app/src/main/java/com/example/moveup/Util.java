aaf8756c0f24e1066e8694e357cd40332fce6002
YimingJ <jiangyimings@gmail.com>
2017-10-08 20:29:54 +1100

    modify suggestion message

--- MoveUp/app/src/main/java/com/example/moveup/Util.java
+++ MoveUp/app/src/main/java/com/example/moveup/Util.java
@@ -5,12 +5,12 @@ package com.example.moveup;
  */
 
 public class Util {
-    public static final String category1 = "Underweight";
-    public static final String category2 = "Normal (healthy weight)";
-    public static final String category3 = "Overweight";
-    public static final String category4 = "Obese Class I (Moderately obese)";
-    public static final String category5 = "Obese Class II (Severely obese)";
-    public static final String category6 = "Obese Class III (Very severely obese)";
+    public static final String category1 = "YourBMI result shows you are underweight. It is likely to lead to poor immunity, gallstones and anaemia. We suggest you pay attention to nutrition balance and have a healthy diet. Try the stretch exercise!";
+    public static final String category2 = "Your BMI result shows you are in a healthy weight. Congratulations! Reasonable exercise and diet can keep you in shape. Try some strength exercise!";
+    public static final String category3 = "Your BMI result shows you are overweight. It may lead to hyperglycaemia and hypertension. Having more vegetables and less meat are good for you. We suggest you do some yoga exercise! ";
+    public static final String category4 = "Your BMI shows you are in obese class I. Moderate obesity carries an increased risk of conditions such as: hypertension, heart disease and type 2 diabetes. Please exercise 4 times a week, at least 40 minutes for each. Try “lose weight” exercise!";
+    public static final String category5 = "Your BMI shows you are in obese class II. Medium obesity carries an increased risk of conditions such as: hypertension, heart disease and type 2 diabetes. Please exercise 4 times a week, at least 40 minutes for each. Try “lose weight” exercise!";
+    public static final String category6 = "Your BMI shows you are in obese class II. Medium obesity carries an increased risk of conditions such as: hypertension, heart disease and type 2 diabetes. Please exercise 4 times a week, at least 40 minutes for each. We strongly recommend you seek a medication assistance.";
 
 
     public static Double calculateBMI(int height, int weight) {
